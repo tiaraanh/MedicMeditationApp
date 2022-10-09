c3ac0f09880e7cfb3b347137f3878cd5e0f558fd
Tiara <tiaranurulh@gmail.com>
2022-10-10 00:27:40 +0700

    Initial Commit

---
+++ MedicMeditationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedicMeditationApp
+//
+//  Created by Tiara H on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
